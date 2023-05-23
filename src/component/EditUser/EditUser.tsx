fcbc742e70946ab01d519df38c7ab8d3dce24f1d
AlexFreyjr <alexfreyjr@gmail.com>
2023-05-23 16:16:54 +0200

    90% done

--- src/component/EditUser/EditUser.tsx
+++ src/component/EditUser/EditUser.tsx
@@ -14,47 +14,69 @@ function EditUser(): JSX.Element {
 	const token = useSelector(selectToken)
 	const [newFirstName, setNewFirstName] = useState(firstName)
 	const [newLastName, setNewLastName] = useState(lastName)
+	const [edit, setEdit] = useState(false)
 	const dispatch = useDispatch()
+
+	const handleEdit = () => {
+		setEdit(true)
+	}
+	const handleCancel = () => {
+		setEdit(false)
+	}
 
 	async function handleSave(e: any) {
 		e.preventDefault()
 		const data = await modifyProfile(token, newFirstName, newLastName)
-		console.log(data)
 		if (!data) alert("Error while charging user please retry")
 		else {
 			const { firstName, lastName } = data.body
 			dispatch(modifyFirstName(firstName))
 			dispatch(modifyLastName(lastName))
 		}
+		setEdit(false)
 	}
-	return (
-		<div className={styles.header}>
-			<h1>
-				Welcome back
-				<br />
-				<div className={styles.input_wrapper}>
-					<input
-						type='text'
-						placeholder={firstName}
-						value={newFirstName}
-						onChange={(e) => setNewFirstName(e.target.value)}
-					/>
-				</div>
-				<div className={styles.input_wrapper}>
-					<input
-						type='text'
-						placeholder={lastName}
-						value={newLastName}
-						onChange={(e) => setNewLastName(e.target.value)}
-					/>
-				</div>
-				<button className={styles.save_button} onClick={handleSave}>
-					Save
+	if (edit) {
+		return (
+			<div className={styles.header}>
+				<h1>
+					Welcome back
+					<br />
+					<div className={styles.input_wrapper}>
+						<input
+							type='text'
+							placeholder={firstName}
+							value={newFirstName}
+							onChange={(e) => setNewFirstName(e.target.value)}
+						/>
+						<input
+							type='text'
+							placeholder={lastName}
+							value={newLastName}
+							onChange={(e) => setNewLastName(e.target.value)}
+						/>
+					</div>
+					<button className={styles.save_button} onClick={handleSave}>
+						Save
+					</button>
+					<button className={styles.cancel_button} onClick={handleCancel}>
+						Cancel
+					</button>
+				</h1>
+			</div>
+		)
+	} else
+		return (
+			<div className={styles.header}>
+				<h1>
+					Welcome back
+					<br />
+					{firstName} {lastName}!
+				</h1>
+				<button className={styles.edit_button} onClick={handleEdit}>
+					Edit Name
 				</button>
-				<button className={styles.cancel_button}>Cancel</button>
-			</h1>
-		</div>
-	)
+			</div>
+		)
 }
 
 export default EditUser
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,27 +1,11 @@
-import { useSelector } from "react-redux"
 import EditUser from "../../component/EditUser/EditUser"
-import { selectProfile } from "../../utils/slices/profileSlice"
 import Transactions from "../Transactions/Transactions"
 import styles from "./Profile.module.scss"
 
 function Profile(): JSX.Element {
-	const { firstName, lastName } = useSelector(selectProfile)
-
-	const handleEdit = () => {
-		return <EditUser />
-	}
 	return (
 		<main className={styles.bg_dark}>
-			<div className={styles.header}>
-				<h1>
-					Welcome back
-					<br />
-					{firstName} {lastName}!
-				</h1>
-				<button className={styles.edit_button} onClick={handleEdit}>
-					Edit Name
-				</button>
-			</div>
+			<EditUser />
 			<Transactions />
 		</main>
 	)
